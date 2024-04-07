5163cf24072e5f0961e23036e130699516e50426
Ousmane <98632155+Ousmane-T@users.noreply.github.com>
2024-04-07 01:50:19 -0400

    Implement the logic upon receive the POST request and add the postman test json file

--- BuberBreakfast/Controllers/BreakfastController.cs
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -1,20 +1,30 @@
 using BuberBreakfast.Contracts.Breakfast;
 using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
 
 
 [ApiController]
-[Route("[controller]")]
+[Route("/breakfasts")]
 public class BreakfastController : ControllerBase
 {
-    [HttpPost]
+    private readonly IBreakfastService _breakfastService;
+
+    public BreakfastController(IBreakfastService breakfastService)
+    {
+        _breakfastService = breakfastService;
+    }
+
+    [HttpPost()]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         var breakfast = new Breakfast(Guid.NewGuid(), request.Name, request.Description, request.StartDateTime,
             request.EndDateTime, DateTime.UtcNow, request.Savory, request.Sweet);
 
+        _breakfastService.CreateBreakfast(breakfast);
+        
         var response = new BreakfastResponse(breakfast.Id, breakfast.Name, breakfast.Description,
             breakfast.StartDateTime, breakfast.EndDateTime, breakfast.LastModifiedDateTime, breakfast.Savory,
             breakfast.Sweet);
@@ -25,7 +35,13 @@ public class BreakfastController : ControllerBase
     [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
-        return Ok(id);
+        Breakfast breakfast = _breakfastService.GetBreakfast(id);
+        
+        var response = new BreakfastResponse(breakfast.Id, breakfast.Name, breakfast.Description,
+            breakfast.StartDateTime, breakfast.EndDateTime, breakfast.LastModifiedDateTime, breakfast.Savory,
+            breakfast.Sweet);
+        
+        return Ok(response);
     }
 
     [HttpPut("{id:guid}")]
--- BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
+++ BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -6,5 +6,6 @@ namespace BuberBreakfast.Services.Breakfasts;
 public interface IBreakfastService
 {
     void CreateBreakfast(Breakfast breakfast);
-    
+
+    Breakfast GetBreakfast(Guid id);
 }
